9dc482d8d464312e712d6942cd79c6925b2d1334
patelana <anand.patel@mail.utoronto.ca>
2017-02-23 02:03:53 -0500

    fully functional

--- timer.js
+++ timer.js
@@ -1,31 +1,58 @@
+/**
+ * called when page loads to initialize the main functions.
+ */
 function init() {
 	addMessageListeners();
 	startTimer();
+	stopOnClick();
 }
 
+/**
+ * sends mesage to start the timer.
+ */
 function startTimer() {
 	// sends message to background.js
-	chrome.runtime.sendMessage({"command": "startTimer"},
+	chrome.runtime.sendMessage({"ask": "current"},
 		function(response) {
 			// response callback
-			console.log(response.message);
-		})
+			if (response.current === "popup.html") {
+				chrome.runtime.sendMessage({"command": "startTimer"});
+			};
+		});
 }
 
+/**
+ * updates the time in the timer.html DOM.
+ */
 function updateTime(timeDifference) {
 	pomTimer = document.getElementById('time').innerText = timeDifference;
-	// console.log(pomTimer);
 }
 
+function updateLabel(label) {
+	document.getElementById("label").innerHTML = label;
+}
+
+/**
+ * adds message listener to update the time.
+ */
 function addMessageListeners() {
 	chrome.runtime.onMessage.addListener(
 		function(request, sender, sendResponse) {
 			if (request.command === 'updateTime') {
 				updateTime(request.time);
-			}
+			};
+
+			if (request.update) {
+				updateLabel(request.update);
+			};
 		});
 }
 
+function stopOnClick() {
+	document.getElementById("stopBtn").addEventListener('click', function(e) {
+		chrome.runtime.sendMessage({"command": "stopAll"});
+		window.alert(states);
+	});
+}
 
-// var pomTimer = document.getElementById('time').innerText;
 document.addEventListener('DOMContentLoaded', init);
